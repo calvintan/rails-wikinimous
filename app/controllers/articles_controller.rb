6a47592824997cb555527aab2ac541761a64e408
Calvin Tan <hello@calvin-tan.com>
2019-07-23 16:26:14 +0800

    Add basic routes and controllers, minimal styling

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,20 +1,25 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
   end
 
   def show
+    @articles = Article.find(params[:id])
   end
 
   def edit
+    @articles = Article.find(params[:id])
   end
 
   def update
+    @articles = Article.find(params[:id])
   end
 
   def create
   end
 
   def destroy
+    @articles = Article.find(params[:id])
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,3 @@
 Rails.application.routes.draw do
-  get 'articles/index'
-  get 'articles/show'
-  get 'articles/edit'
-  get 'articles/update'
-  get 'articles/create'
-  get 'articles/destroy'
-  get 'articles/new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :articles
 end
